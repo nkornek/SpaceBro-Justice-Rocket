9ef3690cb6cc22085d8c7d54f592153d94c16b7a
nkornek <nkornek@gmail.com>
2014-07-09 10:46:13 -0400

    Began new UI

--- HeyBro/Assets/Scripts/Sequence_Queue.cs
+++ HeyBro/Assets/Scripts/Sequence_Queue.cs
@@ -4,64 +4,56 @@ using System.Collections;
 public class Sequence_Queue : MonoBehaviour {
 
 	public GameObject[] sequenceObjects;
-	private Sprite[] sequenceSprites;
+	private Texture[] sequenceSprites;
 	
-	public Sprite pictogramHighfive;
-	public Sprite pictogramFist;
-	public Sprite pictogramElbow;
+	public Texture pictogramHighfive, pictogramFist, pictogramElbow;
 
 	// delay stuff
 	public SequenceControls player; 
 	public float seqDelay;
 	public float currentTime;
-	public float yDistanceBetweenPictograms;
-	public int yTicksBetweenPositions;
+	public float zDistanceBetweenPictograms;
 	
-	public bool movingSpritesDown;
+	public bool movingSpritesForward;
 	
 	// Use this for initialization
 	void Start () {
-		sequenceSprites = new Sprite[3] {pictogramHighfive, pictogramFist, pictogramElbow};
-		yDistanceBetweenPictograms = 1.0f;
-		yTicksBetweenPositions = 4;
+		sequenceSprites = new Texture[3] {pictogramHighfive, pictogramFist, pictogramElbow};
+		zDistanceBetweenPictograms = 0.1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-	
+	/*
 	void FixedUpdate () {
 		currentTime += Time.deltaTime;
 		MoveSpriteDown();
 	}
+	*/
 	
 	// Sets the appropriate pictogram sprites and visibility
 	public void LoadSequence (int[] seq, float delay) {
-		currentTime = 0f;
 		for (int i = 0; i < seq.Length; i++) {
 			sequenceObjects[i].renderer.enabled = true;
-			sequenceObjects[i].transform.localPosition = new Vector3 (0, yDistanceBetweenPictograms*(i+1), 0);
-			sequenceObjects[i].GetComponent<SpriteRenderer>().sprite = sequenceSprites[seq[i]];
+			sequenceObjects[i].transform.localPosition = new Vector3 (0, 0, - zDistanceBetweenPictograms*(i+1));
+			sequenceObjects[i].guiTexture.texture = sequenceSprites[seq[i]];
 		}
 		
 		for (int i = seq.Length; i < sequenceObjects.Length; i++) {
 			sequenceObjects[i].renderer.enabled = false;
 		}
-		//Multiplying delay for easier debugging
-		seqDelay = 5.0f*delay / yTicksBetweenPositions;
+
 	}
-	public void MoveSpriteDown(){
-		float yTranslation = yDistanceBetweenPictograms / (float) yTicksBetweenPositions;
+	public void MoveSpriteForward(){
+		float zTranslation = zDistanceBetweenPictograms;
 		
-		if (movingSpritesDown){
-			if (currentTime >= seqDelay){
+		if (movingSpritesForward){
 				foreach (GameObject o in sequenceObjects) {
-					o.gameObject.transform.position = new Vector3 (o.gameObject.transform.position.x, o.gameObject.transform.position.y - yTranslation, 
-					o.gameObject.transform.position.z);
-				}
-				currentTime = 0;				
+					o.gameObject.transform.position = new Vector3 (o.gameObject.transform.position.x, o.gameObject.transform.position.y, 
+					o.gameObject.transform.position.z - zTranslation);
+				}			
 			}
 		}
-	}
 }
