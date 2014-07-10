d3728b73bfeb7b3e93946dcb720367d8c641834b
Roy Baron <roy@decodeglobal.com>
2014-07-10 13:59:56 -0400

    healthbar fade in/out

--- HeyBro/Assets/Scripts/HealthBarEnemy.cs
+++ HeyBro/Assets/Scripts/HealthBarEnemy.cs
@@ -10,12 +10,17 @@ public class HealthBarEnemy : MonoBehaviour {
 	public float curPerc;
 	public float targetPerc;
 	public GameObject barLeft, healthBar;
+	public bool CanFadeIn, fadeSwitch;
+	public float alpha;
 
 	// Use this for initialization
 	void Start () {
 		enemy = GameObject.Find ("Enemy").GetComponent<EnemyControls>();
 		curPerc = 0.1f;
 		targetPerc = 1f;
+		CanFadeIn = true;
+		alpha = 0f;
+		fadeSwitch = true;
 	}
 	
 	// Update is called once per frame
@@ -23,7 +28,20 @@ public class HealthBarEnemy : MonoBehaviour {
 		//float percHealth = (float) enemy.hp / (float) enemy.maxHP
 		//transform.localScale = new Vector3 (max_XScale * percHealth, yScale, 1f);
 		targetPerc = (float) enemy.hp / (float) enemy.maxHP;
-		
+		if (CanFadeIn & alpha < 1)
+		{
+			alpha += 0.05f;
+		}
+		else if (!CanFadeIn & alpha > 0)
+		{
+			alpha -= 0.05f;
+		}
+
+		foreach (SpriteRenderer r in GetComponentsInChildren<SpriteRenderer>())
+		{
+			r.color = new Color (1.0f, 1.0f, 1.0f, alpha);
+		}
+
 		if (targetPerc < 0f) {
 			targetPerc = 0f;
 		}
@@ -32,12 +50,26 @@ public class HealthBarEnemy : MonoBehaviour {
 		
 			if (Mathf.Abs (curPerc - targetPerc) <= 0.001f) {
 				curPerc = targetPerc;
+				if (fadeSwitch & CanFadeIn)
+				{
+					fadeSwitch = false;
+					Invoke ("FadeOutStart", 0.7f);
+				}
 			}
 			else {
+				CanFadeIn = true;
+				if (alpha > 0.9f)
+				{
 				curPerc = Mathf.Lerp (curPerc, targetPerc, 0.03f);
+				}
+
 			}			
 			healthBar.transform.localScale = new Vector3 (max_XScale * curPerc, yScale, 1f);
 			barLeft.transform.localPosition = new Vector3 ( -1.84f - (max_XScale * curPerc * 1.13333333333333333f), -0.87f, 0);
 		}		
+	}
+public void FadeOutStart(){
+		CanFadeIn = false;
+		fadeSwitch = true;
 	}
 }
--- HeyBro/Assets/Scripts/HealthBarPlayer.cs
+++ HeyBro/Assets/Scripts/HealthBarPlayer.cs
@@ -10,12 +10,17 @@ public class HealthBarPlayer : MonoBehaviour {
 	public float curPerc;
 	public float targetPerc;
 	public GameObject barRight, healthBar;
+	public bool CanFadeIn, fadeSwitch;
+	public float alpha;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Players").GetComponent<SequenceControls>();
 		curPerc = 0.1f;
 		targetPerc = 1f;
+		CanFadeIn = true;
+		alpha = 0f;
+		fadeSwitch = true;
 	}
 	
 	// Update is called once per frame
@@ -23,7 +28,20 @@ public class HealthBarPlayer : MonoBehaviour {
 		//float percHealth = (float) enemy.hp / (float) enemy.maxHP
 		//transform.localScale = new Vector3 (max_XScale * percHealth, yScale, 1f);
 		targetPerc = (float) player.hp / (float) player.maxHP;
+		if (CanFadeIn & alpha < 1)
+		{
+			alpha += 0.05f;
+		}
+		else if (!CanFadeIn & alpha > 0)
+		{
+			alpha -= 0.05f;
+		}
 		
+		foreach (SpriteRenderer r in GetComponentsInChildren<SpriteRenderer>())
+		{
+			r.color = new Color (1.0f, 1.0f, 1.0f, alpha);
+		}
+
 		if (targetPerc < 0f) {
 			targetPerc = 0f;
 		}
@@ -32,12 +50,25 @@ public class HealthBarPlayer : MonoBehaviour {
 			
 			if (Mathf.Abs (curPerc - targetPerc) <= 0.001f) {
 				curPerc = targetPerc;
+				if (fadeSwitch & CanFadeIn)
+				{
+					fadeSwitch = false;
+					Invoke ("FadeOutStart", 0.7f);
+				}
 			}
 			else {
-				curPerc = Mathf.Lerp (curPerc, targetPerc, 0.03f);
+				CanFadeIn = true;
+				if (alpha > 0.9f)
+				{
+					curPerc = Mathf.Lerp (curPerc, targetPerc, 0.03f);
+				}
 			}			
 			healthBar.transform.localScale = new Vector3 (max_XScale * curPerc, yScale, 1f);
 			barRight.transform.localPosition = new Vector3 ( 3.88f + (max_XScale * curPerc * 1.13f), 0.554559f, 0);
 		}		
+	}
+public void FadeOutStart(){
+		CanFadeIn = false;
+		fadeSwitch = true;
 	}
 }
