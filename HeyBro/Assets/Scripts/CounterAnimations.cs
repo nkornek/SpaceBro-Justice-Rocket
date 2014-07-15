b9c446f647db3d3dfb41edd5e3ed8b36add14403
nkornek <nkornek@gmail.com>
2014-07-15 13:44:24 -0400

    working on counter sequence

--- HeyBro/Assets/Scripts/CounterAnimations.cs
+++ HeyBro/Assets/Scripts/CounterAnimations.cs
@@ -2,15 +2,49 @@
 using System.Collections;
 
 public class CounterAnimations : MonoBehaviour {
-	public Animation counterBall;
+	public GameObject energyBall, energyBallObject;
+	public Animator ballCounterAnimator;
+	public SplineNode[] ballSplines;
+	public SmoothFollow counterCamera;
+	public Transform playerTransform, enemyTransform;
+	public bool canTrack;
 
 	// Use this for initialization
 	void Start () {
+		canTrack = false;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (canTrack)
+		{
+			if (energyBallObject.transform.localPosition.x > -15.0f & energyBallObject.transform.localPosition.x < -2.0f)
+			{
+				counterCamera.target = energyBallObject.transform;
+			}
+			else if (energyBallObject.transform.localPosition.x < -2.0f)
+			{
+				counterCamera.target = playerTransform;
+			}
+		}
+		else
+		{
+			counterCamera.target = enemyTransform;
+		}
+	}
+
+	public void FireBall (){
+		energyBallObject.GetComponent<SplineController> ().enabled = true;
+		energyBallObject.GetComponent<SpriteRenderer> ().enabled = true;
+		ballCounterAnimator.SetBool ("hasFired", true);
+		canTrack = true;
+	}
+
+	public void ReflectedBall () {
+		foreach (SplineNode s in ballSplines)
+		{
+			s.speed += 5;
+		}
 	}
 }
