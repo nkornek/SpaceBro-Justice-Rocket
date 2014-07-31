cc57584694409425c2a736ae43bada64cb3a5632
nkornek <nkornek@gmail.com>
2014-07-31 16:52:17 -0400

    more arduino stuff

--- High_five2_0/High_five2_0.ino
+++ High_five2_0/High_five2_0.ino
@@ -1,6 +1,7 @@
 
 int threshhold = 1023;
 int state[6] = {0, 0, 0, 0, 0, 0};
+int willDisplay[6] = {0, 0, 0, 0, 0, 0};
 
 void setup() {
   
@@ -12,12 +13,17 @@ void loop() {
   {
     if (analogRead(pin) >= threshhold & state[pin] == 0)
     {
-      Serial.println (pin+1);
-      state[pin] = 1;
+      willDisplay[pin] += 1;
+      if (willDisplay[pin] >= 5)
+      {
+        Serial.println (pin+1);
+        state[pin] = 1;
+      }
     }
     else if (analogRead(pin) < threshhold & state[pin] == 1)
     {
       state[pin] = 0;
+      willDisplay[pin] = 0;
     }
   }
   delay(5);
