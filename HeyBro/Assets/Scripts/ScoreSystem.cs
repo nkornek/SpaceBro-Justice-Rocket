900460c482e986610ece12cc1ac220e74b09c74a
nkornek <nkornek@gmail.com>
2014-08-01 14:01:24 -0400

    score and webcam tests

---
+++ HeyBro/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreSystem : MonoBehaviour {
+	public int hiScore, score;
+
+
+	void Start() {
+		hiScore = PlayerPrefs.GetInt ("HighScore");
+	}
+
+	void Update() {
+	
+	
+	}
+}
---
+++ HeyBro/Assets/Webcam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webcam : MonoBehaviour {
+
+	public WebCamTexture webcamTexture;
+	public Color32[] data;
+	void Start() {
+		webcamTexture = new WebCamTexture();
+		webcamTexture.Play();
+		data = new Color32[webcamTexture.width * webcamTexture.height];
+	}
+	void Update() {
+		webcamTexture.GetPixels32(data);
+	}
+
+}
